32c8af8a6deba3ffa8a8212756a3ea460faf974e
Shuhao Zhang <IDSZS@nus.edu.sg>
2019-01-09 14:30:04 +0800

    rerun OB.

--- common/src/main/java/applications/CONTROL.java
+++ common/src/main/java/applications/CONTROL.java
@@ -19,8 +19,8 @@ public interface CONTROL {
 
     //engine related.
     boolean enable_mvcc = false; // always enabled in CT and disabled in others.
-    boolean enable_numa_placement = true;//1. numa placement.
-    boolean enable_engine = true;//2. enable TP_engine.
+    boolean enable_engine = true;//1. enable TP_engine.
+    boolean enable_numa_placement = false;//2. numa placement.
     boolean enable_work_stealing = false;//3. this is a sub-option, only useful when engine is enabled.
     boolean enable_speculative = false;//work in future!
 
