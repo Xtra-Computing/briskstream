31fd9dfbaa5fc1fd47ec64b40be13a598cf3e65e
Shuhao Zhang <IDSZS@nus.edu.sg>
2019-01-10 17:18:24 +0800

    rewrite MB.

--- Database/src/main/java/engine/storage/EventManager.java
+++ Database/src/main/java/engine/storage/EventManager.java
@@ -1,19 +1,27 @@
 package engine.storage;
 
 public class EventManager {
-    public Object[] input_events;
-//    private int total_num_events = 0;
+    public volatile Object[] input_events;
+    private int num_events;
+    private int insert_num_events = 0;
 
     public void ini(int num_events) {
         input_events = new Object[num_events];
-//        this.total_num_events = num_events;
+        this.num_events = num_events;
     }
 
     public Object get(int bid) {
+        if (insert_num_events != num_events) {
+            System.out.println("Wrongly insert!");
+        }
+        if (bid > num_events) {
+            System.out.println("Too many bid!" + bid);
+        }
         return input_events[bid];
     }
 
     public void put(Object event, int i) {
         input_events[i] = event;
+        insert_num_events++;
     }
 }
