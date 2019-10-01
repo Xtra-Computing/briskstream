d410e23446d1ccacff006279dbae1378221fa500
shuhaozhangtony <tonyzhang19900609@gmail.com>
2019-10-01 13:44:41 +0800

    Clean
    Clean

--- CEP/src/main/resources/application/Q2/Q2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script file is used to run Q2.
-dirname=Q2_results_$(date +%Y%m%d-%T)
-mkdir $dirname
-
-
-echo "Q2"
-sase application/Q2/Q2.query application/Q2/Q2.stream 1  >>$dirname/Q2.result
