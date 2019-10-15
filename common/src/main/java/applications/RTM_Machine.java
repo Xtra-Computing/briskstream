93e20d9dd6f2934402a0890167917a670b5db1e5
shuhaozhangtony <tonyzhang19900609@gmail.com>
2019-10-15 17:20:03 +0800

    add RTM machine.

--- common/src/main/java/applications/RTM_Machine.java
+++ common/src/main/java/applications/RTM_Machine.java
@@ -13,11 +13,17 @@ public class RTM_Machine extends Platform {
         this.CoresPerSocket = 10;//cores per socket
         this.num_cores = 40;
         bandwidth_map = new double[][]{  //in MB/s
-
+                {20564.8, 9944.0, 9284.1, 9773.4},
+                {9892.5, 20705.0, 9310.0, 9822.4},
+                {9892.5, 9310.0, 20705.0, 9822.4},
+                {9892.5, 9310.0, 9822.4, 20705.0},
         };
 
         latency_map = new double[][]{// in ns
-
+                {142.6, 327.5, 313.4, 316.0},
+                {328.7, 145.6, 322.2, 324.1},
+                {327.5, 328.6, 148.0, 325.5},
+                {327.7, 327.0, 322.2, 145.1},
         };
     }
 }
